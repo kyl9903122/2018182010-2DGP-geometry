23574c264200fe1ff3ceb2177d92c7343a2adad4
kyl990312 <56106969+kyl990312@users.noreply.github.com>
2019-11-06 03:11:44 +0900

    커밋 ??
    배경음 추가

--- 2DGP - geometry dash/background_class.py
+++ 2DGP - geometry dash/background_class.py
@@ -7,7 +7,9 @@ class BACKGROUND:
         self.image_3 = load_image('background1.png')
         self.pivot_1_x, self.pivot_2_x, self.pivot_3_x = 255, 765, 1275
         self.pivot_y = 255
-
+        self.bgm = load_music("Geomatry_resource_backmusic.mp3")
+        self.bgm.set_volume(64)
+        self.bgm.repeat_play()
     def Move(self, speed):
         if (self.pivot_1_x <= -255):
             self.pivot_1_x = self.pivot_3_x + 510
